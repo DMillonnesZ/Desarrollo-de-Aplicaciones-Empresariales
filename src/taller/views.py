731f7cad79d08bc1bcbd33a7f40aadd37a08f583
Daniel Millones <dmillonesz@gmail.com>
2026-08-31 20:34:43 -0500

    Implementar listado de citas con View, URL y Template heredando de base.html (Bootstrap)

--- src/taller/views.py
+++ src/taller/views.py
@@ -1,1 +1,23 @@
 from django.shortcuts import render
+from .models import citas, calcular_presupuesto
+
+
+def lista_citas(request):
+    """
+    Recorre la lista de citas (en memoria) y calcula el presupuesto
+    de cada una antes de enviarlas al template.
+    """
+    citas_con_presupuesto = []
+    for cita in citas:
+        vehiculo = cita["cliente"]["vehiculo"]
+        presupuesto = calcular_presupuesto(vehiculo["servicios"])
+        citas_con_presupuesto.append({
+            "id": cita["id"],
+            "cliente": cita["cliente"],
+            "presupuesto": presupuesto,
+        })
+
+    contexto = {
+        "citas": citas_con_presupuesto,
+    }
+    return render(request, "taller/lista_citas.html", contexto)
