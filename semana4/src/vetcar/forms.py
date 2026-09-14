4620ca5b1f178402ce3d2c8d4e197f0e39e2017b
Daniel Millones <dmillonesz@gmail.com>
2026-09-14 00:40:16 -0500

    feat(forms): crear formularios completos para todas las entidades

--- semana4/src/vetcar/forms.py
+++ semana4/src/vetcar/forms.py
@@ -1,38 +1,112 @@
 from django import forms
-from .models import Dueno, Raza, Veterinario, Mascota, Consulta
+from .models import (
+    Dueno, Veterinario, Raza, Mascota, Consulta, Vacunacion,
+    FichaClinica, SeguimientoClinico, DetalleReceta, Medicamento,
+)
 
 
-class DuenoForm(forms.ModelForm):
+class ClinicaModelForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for campo in self.fields.values():
+            widget = campo.widget
+            clases_previas = widget.attrs.get('class', '')
+            if isinstance(widget, (forms.CheckboxInput,)):
+                nueva_clase = 'form-check-input'
+            elif isinstance(widget, (forms.CheckboxSelectMultiple, forms.RadioSelect)):
+                nueva_clase = 'form-check-input'
+            elif isinstance(widget, (forms.Select, forms.SelectMultiple)):
+                nueva_clase = 'form-select'
+            else:
+                nueva_clase = 'form-control'
+            widget.attrs['class'] = f'{clases_previas} {nueva_clase}'.strip()
+
+
+class DuenoForm(ClinicaModelForm):
     class Meta:
         model = Dueno
-        fields = '__all__'
+        fields = ['nombre', 'telefono', 'correo', 'dni', 'direccion', 'activo']
 
 
-class RazaForm(forms.ModelForm):
-    class Meta:
-        model = Raza
-        fields = '__all__'
-
-
-class VeterinarioForm(forms.ModelForm):
+class VeterinarioForm(ClinicaModelForm):
     class Meta:
         model = Veterinario
-        fields = '__all__'
+        fields = ['nombre', 'telefono', 'correo', 'colegiatura', 'especialidades', 'fecha_ingreso', 'activo']
+        widgets = {
+            'fecha_ingreso': forms.DateInput(attrs={'type': 'date'}),
+            'especialidades': forms.CheckboxSelectMultiple(),
+        }
 
 
-class MascotaForm(forms.ModelForm):
+class RazaForm(ClinicaModelForm):
+    class Meta:
+        model = Raza
+        fields = ['nombre', 'especie', 'descripcion']
+
+
+class MascotaForm(ClinicaModelForm):
     class Meta:
         model = Mascota
-        fields = '__all__'
+        fields = ['nombre', 'fecha_nacimiento', 'sexo', 'peso_kg', 'dueno', 'raza', 'activo']
         widgets = {
             'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['raza'].queryset = self.fields['raza'].queryset.order_by('especie__nombre', 'nombre') # type: ignore[attr-defined]
+        self.fields['dueno'].queryset = self.fields['dueno'].queryset.order_by('nombre') # type: ignore[attr-defined]
 
-class ConsultaForm(forms.ModelForm):
+
+class ConsultaForm(ClinicaModelForm):
     class Meta:
         model = Consulta
-        fields = '__all__'
+        fields = ['mascota', 'veterinario', 'fecha', 'motivo', 'diagnostico', 'tratamiento', 'estado', 'costo']
         widgets = {
             'fecha': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
+
+
+class VacunacionForm(ClinicaModelForm):
+    class Meta:
+        model = Vacunacion
+        fields = ['mascota', 'veterinario', 'fecha', 'nombre_vacuna', 'proxima_dosis', 'lote']
+        widgets = {
+            'fecha': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'proxima_dosis': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+
+class MedicamentoForm(ClinicaModelForm):
+    class Meta:
+        model = Medicamento
+        fields = ['nombre', 'descripcion', 'stock', 'precio_unitario']
+
+
+class FichaClinicaForm(ClinicaModelForm):
+    class Meta:
+        model = FichaClinica
+        fields = ['alergias', 'condiciones_cronicas', 'seguro_veterinario', 'contacto_emergencia', 'observaciones']
+
+class SeguimientoClinicoForm(ClinicaModelForm):
+    class Meta:
+        model = SeguimientoClinico
+        fields = ['veterinario', 'fecha_asignacion', 'rol', 'activo']
+        widgets = {
+            'fecha_asignacion': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['veterinario'].queryset = self.fields['veterinario'].queryset.order_by('nombre') # type: ignore[attr-defined]
+
+
+class DetalleRecetaForm(ClinicaModelForm):
+    class Meta:
+        model = DetalleReceta
+        fields = ['medicamento', 'cantidad', 'dosis', 'indicaciones']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['medicamento'].queryset = self.fields['medicamento'].queryset.order_by('nombre') # type: ignore[attr-defined]
