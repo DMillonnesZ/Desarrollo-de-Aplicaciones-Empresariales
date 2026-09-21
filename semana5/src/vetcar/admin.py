614c7f5ce3a132d8e94d4be61f0690f12acbfe23
Daniel Millones <dmillonesz@gmail.com>
2026-09-20 22:21:38 -0500

    fase 7 (Ejercicio 7): exponer SeguimientoClinico dentro de Mascota con TabularInline

--- semana5/src/vetcar/admin.py
+++ semana5/src/vetcar/admin.py
@@ -13,12 +13,17 @@ class FichaClinicaInline(admin.StackedInline):
     max_num = 1
 
 
+class SeguimientoClinicoInline(admin.TabularInline):
+    model = SeguimientoClinico
+    extra = 1
+
+
 @admin.register(Mascota)
 class MascotaAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'raza', 'dueno', 'sexo', 'activo')
     search_fields = ('nombre', 'dueno__nombre', 'dueno__dni')
     list_filter = ('activo', 'sexo', 'raza__especie')
-    inlines = [FichaClinicaInline]
+    inlines = [FichaClinicaInline, SeguimientoClinicoInline]
 
 
 @admin.register(Veterinario)
