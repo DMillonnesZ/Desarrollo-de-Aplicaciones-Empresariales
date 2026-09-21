28dfffff4138175a284e96090943c5573f69a0cb
Daniel Millones <dmillonesz@gmail.com>
2026-09-20 21:29:41 -0500

    fase 3 : registrar todos los modelos de la Semana 4 en el Admin

--- semana5/src/vetcar/admin.py
+++ semana5/src/vetcar/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from .models import (
-    Especie, Especialidad, Dueno, Veterinario, Raza, Mascota,
-    FichaClinica, Medicamento, Consulta, Vacunacion, DetalleReceta,
+    Especie, Especialidad, Persona, Dueno, Veterinario, Raza, Mascota,
+    FichaClinica, Medicamento, Atencion, Consulta, Vacunacion, DetalleReceta,
     SeguimientoClinico, AuditoriaAtencion, AuditoriaMascota,
 )
 
 admin.site.register(Especie)
 admin.site.register(Especialidad)
+admin.site.register(Persona)
 admin.site.register(Dueno)
 admin.site.register(Veterinario)
 admin.site.register(Raza)
 admin.site.register(Mascota)
 admin.site.register(FichaClinica)
 admin.site.register(Medicamento)
+admin.site.register(Atencion)
 admin.site.register(Consulta)
 admin.site.register(Vacunacion)
 admin.site.register(DetalleReceta)
